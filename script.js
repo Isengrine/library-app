aaa5915de0b21d6fddc4d887d2757f82ed5db9da
Isengrine <isengrine@gmail.com>
2023-02-22 00:09:13 -0600

    Add basic header elements

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        return `${title} by ${author}, ${pages} pages, ${read}.`;
-    }
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => {
+    return `${title} by ${author}, ${pages} pages, ${read}.`;
+  }
 }
 
-const book1 = new book("Crime and Punishment", "Dostoievsky", "300", "not read");
+const book1 = new Book("Crime and Punishment", "Dostoievsky", "300", "not read");
 console.log(book1.info());
