d2d0f9da9229d25ebc3cff032f32542b9bebc3ac
Isengrine <isengrine@gmail.com>
2023-02-21 21:36:42 -0600

    Add book object to script

---
+++ script.js
@@ -0,0 +1,12 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        return `${title} by ${author}, ${pages} pages, ${read}.`;
+    }
+}
+
+const book1 = new book("Crime and Punishment", "Dostoievsky", "300", "not read");
+console.log(book1.info());
